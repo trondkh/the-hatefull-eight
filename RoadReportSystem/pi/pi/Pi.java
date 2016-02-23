bae7c2022ba6cc00c6b274ea58cf3cdbb35e582e
trondkh <trondkh@stud.ntnu.no>
2016-02-23 22:44:13 +0100

    parsing json

--- RoadReportSystem/pi/pi/Pi.java
+++ RoadReportSystem/pi/pi/Pi.java
@@ -1,5 +1,35 @@
 package pi;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import json.*;
+
 public class Pi {
+	
+	void readShit()
+	{
+		Reader r;
+		JsonArray items = new JsonArray();
+		try
+		{
+			r = new FileReader("uptown-west.json");
+			items = Json.parse(r).asArray();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		for(JsonValue v : items)
+		{
+			System.out.println(v.toString());
+		}
+	}
+	
+	public static void main(String[] args) {
+		Pi p = new Pi();
+		p.readShit();
+	}
 
 }
