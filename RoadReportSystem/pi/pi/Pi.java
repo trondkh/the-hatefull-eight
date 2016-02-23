42c3da1bc9840f59ef3c0a2b136ce5f8800d0d0c
trondkh <trondkh@stud.ntnu.no>
2016-02-23 23:29:19 +0100

    Parsing "json"...
    Added a random lightweight json parsing library. But it did not work as
    intended. Added a little logic myself that show that it is easy to parse
    it manually if we wish.

--- RoadReportSystem/pi/pi/Pi.java
+++ RoadReportSystem/pi/pi/Pi.java
@@ -1,8 +1,10 @@
 package pi;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
 
 import json.*;
 
@@ -10,20 +12,36 @@ public class Pi {
 	
 	void readShit()
 	{
+		ArrayList<String> allTheLines = new ArrayList<String>();
 		Reader r;
 		JsonArray items = new JsonArray();
 		try
 		{
-			r = new FileReader("uptown-west.json");
-			items = Json.parse(r).asArray();
+			r = new FileReader("./pi/pi/uptown-west.json");
+			BufferedReader br = new BufferedReader(r);
+			String str;
+			while((str = br.readLine())!=null)
+			{
+				allTheLines.add(str);
+			}
+//			items = Json.parse(str).asArray();
+
+			
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
-		for(JsonValue v : items)
+		String[] strings = new String[3];
+		String str;
+		for(int i=0;i<10;i++)
 		{
-			System.out.println(v.toString());
+			str = allTheLines.get(i);
+			strings = str.split(","); 
+			str = strings[0].split(":")[1];
+			str += " at "; 
+			str += strings[1].split(":")[1];
+			System.out.println(str);
 		}
 	}
 	
