36543d0adc0ea90d763cbc63d0bdd1f369b5a1ba
gwib <galinavj@stud.ntnu.no>
2016-03-16 14:08:43 +0100

    Merge branch 'server_area_information' of https://github.com/trondkh/the-hatefull-eight
    # Conflicts:
    #	RoadReportSystem/.classpath
    #	RoadReportSystem/server/server/Server.java

--- RoadReportSystem/server/server/DisplayXMLData.java
+++ RoadReportSystem/server/server/DisplayXMLData.java
@@ -1,7 +1,42 @@
 package server;
 
+import java.util.ArrayList;
+
 /**
  * Created by Galina on 16.03.2016.
  */
-public class DisplayXMLData {
+public class DisplayXMLData extends InformationHandler {
+
+    @Override
+    public String generateResponse(XMLHandler xmlHandler) {
+        String temp = "Response....\n";
+
+        ArrayList<String> tags = new ArrayList<String>();
+        ArrayList<String> values = new ArrayList<String>();
+
+
+        tags.add("temperature");
+        values.add("value");
+
+        tags.add("temperature");
+        values.add("unit");
+
+        tags.add("windSpeed");
+        values.add("name");
+
+        tags.add("pressure");
+        values.add("value");
+
+        tags.add("pressure");
+        values.add("unit");
+
+        ArrayList<String> results = xmlHandler.requestYr(tags, values);
+
+        for(int i = 0; i < tags.size(); i++) {
+            temp += (tags.get(i) + " / " + values.get(i) + ": " + results.get(i)) + "\n";
+        }
+
+
+        return temp;
+    }
 }
