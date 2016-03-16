a852165a93e877f57bf66250f1b6f645ceecc4e8
gwib <galinavj@stud.ntnu.no>
2016-03-16 14:30:12 +0100

    Merge branch 'server_area_information' of https://github.com/trondkh/the-hatefull-eight
    # Conflicts:
    #	RoadReportSystem/.classpath
    #	RoadReportSystem/server/server/Server.java

--- RoadReportSystem/server/server/DisplayXMLData.java
+++ RoadReportSystem/server/server/DisplayXMLData.java
@@ -20,13 +20,37 @@ public class DisplayXMLData extends InformationHandler {
         tags.add("temperature");
         values.add("unit");
 
-        ArrayList<String> results = xmlHandler.requestYr(tags, values);
+        tags.add("rain");
+        values.add("value");
 
+        ArrayList<String> results = xmlHandler.requestYr(tags, values);
+        //problem: do only 2 results instead of 3, need tag to get "rain"
         for(int i = 0; i < tags.size(); i++) {
             temp += (tags.get(i) + " / " + values.get(i) + ": " + results.get(i)) + "\n";
         }
-
-
         return temp;
+    }
+
+
+    public String parseXMLMessage(String str) {
+        String temp = str;
+
+        String[] subs = temp.trim().split(",");
+        double lat = Double.parseDouble(subs[0].trim());
+        double lon = Double.parseDouble(subs[1].trim());
+
+        CoordinateHandler coordinates = new CoordinateHandler(lat, lon);
+
+        XMLFile yr = new XMLFile(true, coordinates.generateYrURL());
+
+        XMLHandler xmlHandler = new XMLHandler(yr, null, coordinates);
+        //xmlHandler.exampleRequest();
+
+        return generateResponse(xmlHandler);
+    }
+
+    public static void main(String[] args) {
+        DisplayXMLData dxd = new DisplayXMLData();
+        System.out.println(dxd.parseXMLMessage("63.415763,10.406500"));
     }
 }
