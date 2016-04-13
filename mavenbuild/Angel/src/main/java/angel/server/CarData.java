d2d74319467f9a1481f1f185acbc32110fd64659
trondkh <trondkh@stud.ntnu.no>
2016-04-13 20:27:26 +0000

    Added maven stuff

---
+++ mavenbuild/Angel/src/main/java/angel/server/CarData.java
@@ -0,0 +1,72 @@
+package server;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CarData {
+	
+	private String message;
+	private boolean roadSlippage;
+	private String licensePlate;
+	private boolean airbag;
+	private Calendar timestamp;
+	
+	public CarData(CarData cd)
+	{
+		this.init(cd.licensePlate);
+		this.roadSlippage = cd.roadSlippage;
+		this.message = cd.message;
+	}
+	
+	public CarData(String message, boolean roadSlippage, boolean airbag, String licensePlate)
+	{
+		this.init(licensePlate);
+		this.roadSlippage = roadSlippage;
+		this.airbag = airbag;
+		this.message = message;
+	}
+
+	private void init(String licensePlate)
+	{
+		this.licensePlate = licensePlate;
+		this.timestamp = Calendar.getInstance();
+		this.message = "";
+		this.roadSlippage = false;
+		this.airbag = false;
+	}
+	
+	public boolean isThisLicensePlate(String licensePlate)
+	{
+		return this.licensePlate.equals(licensePlate);
+	}
+	
+	public int getSeconds()
+	{
+		return (int)(Calendar.getInstance().getTimeInMillis() - timestamp.getTimeInMillis());
+//		LocalDateTime.now();
+	}
+	
+	public boolean isAirbag()
+	{
+		return this.airbag;
+	}
+	
+	public String getLicensePlate(){
+		return licensePlate;
+	}
+	
+	public boolean isSlippery()
+	{
+		return this.roadSlippage;
+	}
+	
+	@Override
+	public String toString()
+	{
+		String retVal = new String("");
+		retVal += "Car seconds: " + this.getSeconds();
+		retVal += " RoadSlippage: " + this.roadSlippage;
+		retVal += " Msg: " + this.message + "\n";
+		return retVal;
+	}
+}
---
+++ mavenbuild/Angel/src/main/java/angel/server/MainServer.java
@@ -0,0 +1,79 @@
+package server;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import Packets.Packet;
+
+
+public class MainServer {
+
+	Map<String,GeographicalArea> norwayDict;
+	Listener listener;
+	GeographicalArea k;
+
+	public static void main(String[] args) {
+//		InformationHandler informationHandler = new InformationHandler();
+//		System.out.println(informationHandler.parseMessage("63.415763,10.406500"));
+		MainServer gam = new MainServer();
+		gam.init();
+		gam.run();
+		
+	}
+	
+	public void init(){
+		listener = new Listener();
+		norwayDict = new HashMap<String,GeographicalArea>();
+		Norway n = new Norway();
+		for(int i=0;i<n.getNumberOfKommuner();i++){
+			// Add to dictionary/ map
+			GeographicalArea kommune = new GeographicalArea(n.getKommuneNumber(i));
+			norwayDict.put(kommune.getName(), kommune);
+		}
+	}
+	
+	public void run(){		
+		while(true){
+			getNewCar(listener.getCarData());
+			Packet packet= createCarPackage();
+			listener.sendCarData(packet);
+			listener.close();
+		}
+	}
+
+	private String getKommune(double latitude, double longditude){
+		CoordinateHandler city = new CoordinateHandler(latitude, longditude); 
+		return city.getCityName();
+	}
+	
+	private void printAreasWithCars(){
+		for(GeographicalArea a:norwayDict.values()){
+			if(a.numberOfCarsInArea()>0){
+				System.out.println(a.toString());
+			}
+		}
+	}
+	
+	private void getNewCar(Packet packet){
+		String kommune = getKommune(packet.getLatitude(),packet.getLongitude());
+		k = norwayDict.get(kommune);
+		k.updateWithCarData(new CarData("bla bla", true, false, packet.getLicensePlate()));
+		printAreasWithCars();
+	}
+	
+	private Packet createCarPackage(){
+		int numAirbag = 0;
+		int icy = 0;
+		
+		for(CarData c : k.cars){
+			if(c.isAirbag()){
+				numAirbag++;
+			}
+			if(c.isSlippery()){
+				icy++;
+			}
+		}
+
+		return new Packet(new ArrayList<String>(), numAirbag, icy, "Road is fine");
+	}
+}
